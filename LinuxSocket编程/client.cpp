9def35b6e234e9bfb40a45355eeaf1ef334e9c35
crossoverpptx <2269041628@qq.com>
2023-07-18 11:35:47 +0800

    update LinuxSocketProgramming

--- LinuxSocket编程/client.cpp
+++ LinuxSocket编程/client.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // 定义了最常用的系统函数：malloc、free等
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/in.h> // 网络地址类型定义：uint16_t、uint32_t、sockaddr_in、sockaddr_in6等
 #include <arpa/inet.h>
 
 #define SERVER_PORT 9991
@@ -14,33 +14,37 @@ int main(int argc, char* argv[]) {
     int sockfd;
     char *message;
     struct sockaddr_in servaddr;
-    int n;
+    int len; // 从服务器读（接收）数据的长度
     char buf[64];
 
     if(argc != 2) {
-        fputs("Usage: ./echo_client message \n", stderr);
+        fputs("Usage: ./client message \n", stderr); // 向指定的文件写入一个字符串
         exit(1);
     }
 
     message = argv[1];
-    printf("message: %s\n", message);
+    printf("send message: %s\n", message);
 
+    // 1.创建套接字
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    memset(&servaddr, '\0', sizeof(struct sockaddr_in));
-
+    memset(&servaddr, '\0', sizeof(struct sockaddr_in)); // 初始化函数。作用是将某一块内存中的内容全部设置为指定的值
+                                                         // 对结构体清零
     servaddr.sin_family = AF_INET;
     inet_pton(AF_INET, SERVER_IP, &servaddr.sin_addr);
     servaddr.sin_port = htons(SERVER_PORT);
 
+    // 2.与服务器建立连接
     connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
+    // 3.向服务器写（发送）数据
     write(sockfd, message, strlen(message));
 
-    n = read(sockfd, buf, sizeof(buf)-1);
-    if(n>0) {
-        buf[n]='\0';
-        printf("receive: %s\n", buf);
+    // 4.从服务器读（接收）数据
+    len = read(sockfd, buf, sizeof(buf)-1);
+    if (len > 0) {
+        buf[len] = '\0';
+        printf("receive from server: %s\n", buf);
     } else {
         perror("error!!!");
     }
--- LinuxSocket编程/server.cpp
+++ LinuxSocket编程/server.cpp
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <ctype.h>
-#include <arpa/inet.h>
+#include <stdio.h> // 标准输入输出
+#include <unistd.h> // 对 POSIX(可移植操作系统接口) 操作系统 API 的访问
+#include <sys/types.h> // 基本系统数据类型
+#include <sys/socket.h> // 用于操作网络套接字
+#include <string.h> // 字符串处理
+#include <ctype.h> // 测试字符是否属于特定的字符类别
+#include <arpa/inet.h> // Linux下网络编程提供信息转换
 
 #define SERVER_PORT 9991
 
 int main() {
 
-    int sock;   // 代表信箱
+    int sock;   // 代表信箱：指向新创建的socket的文件描述符
     struct sockaddr_in server_addr;
 
 
@@ -24,10 +24,10 @@ int main() {
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);    // 监听本地所有IP地址
     server_addr.sin_port = htons(SERVER_PORT);  // 绑定端口号
 
-    // 实现标签贴到收信得信箱上
+    // 3.实现标签贴到收信的信箱上
     bind(sock, (struct sockaddr *)&server_addr,  sizeof(server_addr));
 
-    // 把信箱挂置到传达室，这样，就可以接收信件了
+    // 4.把信箱挂置到传达室，这样，就可以接收信件了
     listen(sock, 128);
 
     // 万事俱备，只等来信
@@ -35,19 +35,20 @@ int main() {
 
 
     int done =1;
-    while(done){
+    while (done) {
         struct sockaddr_in client;
-        int client_sock, len, i;
-        char client_ip[64];
-        char buf[256];
+        int client_sock, len, i; // 与客户端通信的套接字、客户端发送的数据的长度、转换成大写的临时变量
+        char client_ip[64]; // 存储客户端IP地址
+        char buf[256]; // 存储客户端发送的数据
 
+        // 返回一个新的socket文件描述符，用于和客户端通信
         socklen_t  client_addr_len;
         client_addr_len = sizeof(client);
         client_sock = accept(sock, (struct sockaddr *)&client, &client_addr_len);
 
-        // 打印客服端IP地址和端口号
-        printf("client ip: %s\t port : %d\n",
-                 inet_ntop(AF_INET, &client.sin_addr.s_addr,client_ip,sizeof(client_ip)),
+        // 打印客户端IP地址和端口号
+        printf("client ip: %s\t port: %d\n",
+                 inet_ntop(AF_INET, &client.sin_addr.s_addr, client_ip, sizeof(client_ip)),
                  ntohs(client.sin_port));
 
         // 读取客户端发送的数据
@@ -56,19 +57,20 @@ int main() {
         printf("receive[%d]: %s\n", len, buf);
 
         // 转换成大写
-        for(i=0; i<len; i++){
+        for(i = 0; i < len; ++i) {
             /*if(buf[i]>='a' && buf[i]<='z'){
                 buf[i] = buf[i] - 32;
             }*/
             buf[i] = toupper(buf[i]);
         }
 
-
+        // 向客户端写（发送）数据
         len = write(client_sock, buf, len);
-        printf("finished. len: %d\n", len);
+        printf("finished len: %d\n", len);
         
         close(client_sock);
     }
+
     close(sock);    
     return 0;
 }
